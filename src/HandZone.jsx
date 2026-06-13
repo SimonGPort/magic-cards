116c804559e3df511e0827895077bce7b5e6f324
Simon Giroux <simon.giroux.portelance@gmail.com>
2026-06-13 15:36:32 -0400

    Popup in handzone

--- src/HandZone.jsx
+++ src/HandZone.jsx
@@ -1,7 +1,9 @@
 import { useState } from "react";
 import DraggableCard from "./DraggableCard";
+import PopUp from "./PopUp";
 
 function HandZone() {
+  const [Modal, setModal] = useState(undefined);
   const imageTestSmall =
     "https://cards.scryfall.io/small/front/b/d/bd8fa327-dd41-4737-8f19-2cf5eb1f7cdd.jpg?1614638838";
 
@@ -9,29 +11,32 @@ function HandZone() {
     "https://cards.scryfall.io/normal/front/b/d/bd8fa327-dd41-4737-8f19-2cf5eb1f7cdd.jpg?1614638838";
 
   return (
-    <div
-      style={{
-        position: "fixed",
-        bottom: 0,
-        left: 0,
-        width: "100vw",
-        height: "50vh",
-        backgroundColor: "#f5f0e6", // light beige
-        zIndex: 1000,
-        borderTop: "1px solid #ccc",
-      }}
-    >
-      {" "}
-      <DraggableCard
-        popUp={(image) => {
-          setModal(image);
+    <>
+      {Modal && <PopUp image={Modal} onClose={() => setModal(undefined)} />}
+      <div
+        style={{
+          position: "fixed",
+          bottom: 0,
+          left: 0,
+          width: "100vw",
+          height: "50vh",
+          backgroundColor: "#f5f0e6", // light beige
+          zIndex: 1000,
+          borderTop: "1px solid #ccc",
         }}
-        imageCardSmall={imageTestSmall}
-        imageCardNormal={imageTestNormal}
-        startX={100}
-        startY={100}
-      />
-    </div>
+      >
+        {" "}
+        <DraggableCard
+          popUp={(image) => {
+            setModal(image);
+          }}
+          imageCardSmall={imageTestSmall}
+          imageCardNormal={imageTestNormal}
+          startX={100}
+          startY={100}
+        />
+      </div>
+    </>
   );
 }
 
