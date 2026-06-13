0d1243982be790a816cf88df95682c871a703aae
Simon Giroux <simon.giroux.portelance@gmail.com>
2026-06-13 17:35:55 -0400

    card's delete rotation location in handzone

--- src/HandZone.jsx
+++ src/HandZone.jsx
@@ -4,11 +4,40 @@ import PopUp from "./PopUp";
 
 function HandZone() {
   const [Modal, setModal] = useState(undefined);
-  const imageTestSmall =
-    "https://cards.scryfall.io/small/front/b/d/bd8fa327-dd41-4737-8f19-2cf5eb1f7cdd.jpg?1614638838";
+  const [Cards, setCards] = useState([
+    {
+      imageCardSmall:
+        "https://cards.scryfall.io/small/front/b/d/bd8fa327-dd41-4737-8f19-2cf5eb1f7cdd.jpg?1614638838",
+      imageCardNormal:
+        "https://cards.scryfall.io/normal/front/b/d/bd8fa327-dd41-4737-8f19-2cf5eb1f7cdd.jpg?1614638838",
+      posX: 100,
+      posY: 100,
+      rotation: 0,
+    },
+    {
+      imageCardSmall:
+        "https://cards.scryfall.io/small/front/b/d/bd8fa327-dd41-4737-8f19-2cf5eb1f7cdd.jpg?1614638838",
+      imageCardNormal:
+        "https://cards.scryfall.io/normal/front/b/d/bd8fa327-dd41-4737-8f19-2cf5eb1f7cdd.jpg?1614638838",
+      posX: 300,
+      posY: 300,
+      rotation: 0,
+    },
+  ]);
 
-  const imageTestNormal =
-    "https://cards.scryfall.io/normal/front/b/d/bd8fa327-dd41-4737-8f19-2cf5eb1f7cdd.jpg?1614638838";
+  const updateCard = (id, x, y, newRotation) => {
+    setCards((prevCards) =>
+      prevCards.map((card, index) =>
+        index === id
+          ? { ...card, posX: x, posY: y, rotation: newRotation }
+          : card,
+      ),
+    );
+  };
+
+  const deleteCard = (id) => {
+    setCards((prev) => prev.filter((card, index) => index !== id));
+  };
 
   return (
     <>
@@ -25,16 +54,24 @@ function HandZone() {
           borderTop: "1px solid #ccc",
         }}
       >
-        {" "}
-        <DraggableCard
-          popUp={(image) => {
-            setModal(image);
-          }}
-          imageCardSmall={imageTestSmall}
-          imageCardNormal={imageTestNormal}
-          startX={100}
-          startY={100}
-        />
+        {Cards.map((card, id) => {
+          return (
+            <DraggableCard
+              key={id}
+              id={id}
+              updateCard={(x, y, newRotation) =>
+                updateCard(id, x, y, newRotation)
+              }
+              deleteCard={() => deleteCard(id)}
+              popUp={(image) => setModal(image)}
+              imageCardSmall={card.imageCardSmall}
+              imageCardNormal={card.imageCardNormal}
+              posX={card.posX}
+              posY={card.posY}
+              rotation={card.rotation}
+            />
+          );
+        })}
       </div>
     </>
   );
